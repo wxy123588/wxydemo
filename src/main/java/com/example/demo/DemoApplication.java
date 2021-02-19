b79051df2bb7f994dc89a308fa85bcc99a6ac6eb
weixiangyun <weixiangyun@xyzt.onexmail.com>
2021-02-19 09:51:08 +0800

    基础

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,19 @@
 package com.example.demo;
 
+import com.example.demo.mobile.MobileNumberUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+
 @SpringBootApplication
 public class DemoApplication {
-
 	public static void main(String[] args) {
+		try {
+			MobileNumberUtils.init();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
